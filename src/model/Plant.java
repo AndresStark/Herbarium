972d2223008f4faab6aa132617c2030876f6139d
Cloud <andres.sanchez.cloud@gmail.com>
2024-02-06 16:36:55 -0600

    Inicio de proyecto Herbarium

---
+++ src/model/Plant.java
@@ -0,0 +1,12 @@
+package model;
+
+import java.util.ArrayList;
+
+public class Plant {
+    private String name;
+    private float minimumSize;
+    private float maximumSize;
+    private float averageSize = (minimumSize + maximumSize) / 2;
+
+    
+}
